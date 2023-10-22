b90cfd8d017b71a2ae6e17b1d6e00203737ab474
python815 <boyan.blagoevbb@gmail.com>
2023-10-22 21:45:17 +0300

    add record

--- crm_website/views.py
+++ crm_website/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
 
 
 def home(request):
+    records = Record.objects.all()
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -17,7 +20,8 @@ def home(request):
             messages.error(request, 'Invalid username or password')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+
+        return render(request, 'home.html', {'records': records})
 
 
 def login_user(request):
@@ -31,4 +35,71 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and Login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Your account has been created!')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
+
+
+# noinspection PyShadowingNames
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look up record
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, 'You must be logged in to view that page!')
+        return redirect('home')
+
+
+# noinspection PyShadowingNames
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        # Delete record
+        customer_record = Record.objects.get(id=pk)
+        customer_record.delete()
+        messages.success(request, 'Customer record deleted!')
+        return redirect('home')
+    else:
+        messages.success(request, 'You must be logged in to view that page!')
+        return redirect('home')
+
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, 'Customer record added!')
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request, 'You must be logged in to view that page!')
+        return redirect('home')
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Customer record updated!')
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.success(request, 'You must be logged in to view that page!')
+        return redirect('home')
+
